bb919d93f7b58c23635882a0f76221702e942495
William Long <wlong0827@gmail.com>
2016-02-01 00:50:18 -0500

    Trial code

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // This is the main file
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('https://community.alumni.harvard.edu/', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    console.log(html);
+  }
+});
